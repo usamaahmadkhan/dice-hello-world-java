f8cbdd22b9e8423807807c313437d77a002ec503
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2021-07-17 17:03:07 +0500

    Update message

--- src/main/java/com/dice/app/App.java
+++ src/main/java/com/dice/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World Usama!";
+    private final String message = "Hello World Salman!";
 
     public App() {}
 
